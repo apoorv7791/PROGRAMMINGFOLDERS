c86d8f64b5595a0b77eaea41b1a447cbee8acb19
Apoorv Singh <singhapoorv7791@gmail.com>
2026-07-14 16:46:49 +0530

    made a program that tracks frequency for each character

--- MCAJAVA/strength.java
+++ MCAJAVA/strength.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class strength {
 	void main(){
-		int nums[] = {1, 2, 3, 4, 5, 6};
+		int nums[] = {1, 2, 3, 4, 5, 6, 1, 2, 3, 4};
 		var map = new HashMap<Integer, Integer>();
 		for(int i : nums){
 			map.put(i, map.getOrDefault(i, 0) + 1);
@@ -9,6 +9,7 @@ class strength {
 		System.out.println(map);
 	}
 }
+
 
 
 
