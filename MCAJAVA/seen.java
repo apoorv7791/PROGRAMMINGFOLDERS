82765d279fff5372ceba3524478d9902739e12c5
Apoorv Singh <singhapoorv7791@gmail.com>
2026-07-17 18:29:15 +0530

    modified seen.java

--- MCAJAVA/seen.java
+++ MCAJAVA/seen.java
@@ -1,13 +1,16 @@
-import java.util.HashSet;
-
-public class seen {
+import java.util.*;
+class seen {
     void main() {
-        var seen = new HashSet<>();
-        seen.add(1);
-        seen.add(2);
-        seen.add(3);
-        seen.add(1);
-        System.out.println(seen);
-
-    }
+	int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2};
+	var set = new HashSet<Integer>();
+	for(int i : nums){
+	    if (set.contains(i)) {
+		System.out.println("Element found repeating: " + i); 
+	    } else {	
+		set.add(i);
+	    }
+	}
+	System.out.println("Unique Elements: " + set);
+	System.out.println("Original Array: " + Arrays.toString(nums));
+   }
 }
