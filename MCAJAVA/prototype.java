8f502ce1433498229fc8fa1a3f81d728df80bd06
Apoorv Singh <singhapoorv7791@gmail.com>
2026-07-23 18:48:54 +0530

    Added prototype.java

---
+++ MCAJAVA/prototype.java
@@ -0,0 +1,10 @@
+class prototype {
+	void main(){
+	      int nums[] = {1, 2, 3, 4, 5, 6, 7};
+	      int sum = 0;
+	      for(int i = 0; i < nums.length; i++){
+		  sum += nums[i];
+	      }
+	      System.out.println(sum);
+	}
+}
